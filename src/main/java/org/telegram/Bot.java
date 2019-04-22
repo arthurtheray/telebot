c64fcaf0fbbbad4fca82a3285aa948d0f487d77f
Arthur B <master.artur@mail.ru>
2019-04-23 03:20:40 +0500

    Changed Procifile

---
+++ src/main/java/org/telegram/Bot.java
@@ -0,0 +1,61 @@
+package org.telegram;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Bot extends TelegramLongPollingBot {
+
+    private static Logger log = Logger.getLogger(Bot.class.getName());
+
+    public void onUpdateReceived(Update update) {
+
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+
+    }
+
+    public synchronized void sendMsg(String chatId, String message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Хрюхрюхрюхрю"+message);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.log(Level.SEVERE, "Exception: ", e.toString());
+        }
+    }
+//dfdf
+
+    public String getBotUsername() {
+        return "mcDudleBot";
+    }
+
+    public String getBotToken() {
+        return "855976947:AAH-MfKwr5e36uB3G7oscVJpTfweqocL168";
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
